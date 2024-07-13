eec8b238f3da6760ca16f282e8a495bae2831c0f
NaveenPrajapati <solidauto202@gmail.com>
2024-07-13 22:19:30 +0530

    final submit

--- script.js
+++ script.js
@@ -71,96 +71,96 @@ const pdfTools = [
       "Add text, images, shapes or freehand annotations to a PDF document. Edit the size, font, and color of the added content.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 25.svg",
     name: "PDF to JPG",
     description:
       "Convert each PDF page into a JPG or extract all images contained in a PDF.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 26.svg",
     name: "JPG to PDF",
     description:
       "Convert JPG images to PDF in seconds. Easily adjust orientation and margins.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 27.svg",
     name: "Sign PDF",
     description: "Sign yourself or request electronic signatures from others.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 28.svg",
     name: "Watermark",
     description:
       "Stamp an image or text over your PDF in seconds. Choose the typography, transparency and position.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 29.svg",
     name: "Rotate PDF",
     description:
       "Rotate your PDFs the way you need them. You can even rotate multiple PDFs at once!",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 30.svg",
     name: "HTML to PDF",
     description:
       "Convert webpages in HTML to PDF. Copy and paste the URL of the page you want and convert it to PDF with a click.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 31.svg",
     name: "Unlock PDF",
     description:
       "Remove PDF password security, giving you the freedom to use your PDFs as you want.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 32.svg",
     name: "Protect PDF",
     description:
       "Protect PDF files with a password. Encrypt PDF documents to prevent unauthorized access.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 33.svg",
     name: "Organize PDF",
     description:
       "Sort pages of your PDF file however you like. Delete PDF pages or add PDF pages to your document at your convenience.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 34.svg",
     name: "PDF to PDF/A",
     description:
       "Transform your PDF to PDF/A, the ISO-standardized version of PDF for long-term archiving. Your PDF will preserve formatting when accessed in the future.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 35.svg",
     name: "Repair PDF",
     description:
       "Repair a damaged PDF and recover data from corrupt PDF. Fix PDF files with our Repair tool.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 36.svg",
     name: "Page numbers",
     description:
       "Add page numbers into PDFs with ease. Choose your positions, dimensions, typography.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 37.svg",
     name: "Scan to PDF",
     description:
       "Capture document scans from your mobile device and send them instantly to your browser.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 38.svg",
     name: "Compare PDF",
     description:
       "Show a side-by-side document comparison and easily spot changes between different file versions.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 39.svg",
     name: "Redact PDF",
     description:
       "Redact text and graphics to permanently remove sensitive information from a PDF.",
   },
   {
-    img: "../assets/images/feature1.svg",
+    img: "../assets/images/assets/asset 40.svg",
     name: "OCR PDF",
     description:
       "Easily convert scanned PDF into searchable and selectable documents.",
